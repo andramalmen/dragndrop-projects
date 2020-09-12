a2352a5c99f13c0fcf7f84b214227a7a360d0662
Andra Malmen <andra.malmen@tieto.com>
2020-09-12 17:28:45 +0300

    Set inputs as class members and get title value

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLElement;
     formElement: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
@@ -9,7 +12,26 @@ class ProjectInput {
 
         const importedHtmlContent = document.importNode(this.templateElement.content, true);
         this.formElement = importedHtmlContent.firstElementChild as HTMLFormElement;
+        this.formElement.id = 'user-input';
+
+        this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.formElement.querySelector(
+            '#description'
+        ) as HTMLInputElement;
+        this.peopleInputElement = this.formElement.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
+
         this.attach();
+    }
+
+    private configure() {
+        this.formElement.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
     }
 
     private attach() {
